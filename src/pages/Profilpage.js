8caee11c960d383c13262b6e3329be31ae5cd668
Deniansyah <deniansyah814@gmail.com>
2023-02-20 09:29:34 +0700

    integration profile with update it

--- src/pages/Profilpage.js
+++ src/pages/Profilpage.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as Icon from "react-feather";
 import { Link } from "react-router-dom";
 
 import { useDispatch } from "react-redux";
 import { logout as logoutAction } from "../redux/reducers/auth";
+import http from "../helpers/http";
+import { useSelector } from "react-redux";
 
 import Navbarlogin from "../components/Navbarlogin";
 import Footer from "../components/Footer";
 
 const Profilpage = () => {
-  const dispatch = useDispatch()
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
+
+  const [dataUser, setDataUser] = useState({});
+
+  const getUserProfile = async () => {
+    const { data } = await http(token).get("/profile");
+    setDataUser(data.results);
+  };
+
+  const updateUser = async (e) => {
+    e.preventDefault();
+    const values = new URLSearchParams({
+      firstName: e.target.firstName.value,
+      lastName: e.target.lastName.value,
+      email: e.target.email.value,
+      phoneNumber: e.target.phoneNumber.value
+    });
+    setIsLoading(true);
+    await http(token).patch(`/profile`, values);
+    setMessage("Profile Updated");
+    setIsLoading(false);
+    setTimeout(() => {
+      setMessage("");
+    }, [5000]);
+    console.log("ok")
+  };
+
+  useEffect(() => {
+    getUserProfile();
+  });
 
   return (
     <>
@@ -24,12 +58,15 @@ const Profilpage = () => {
                 src={require("../assets/images/profil.png")}
                 alt="profil"
               />
-              <h2 className="font-bold">Jonas El Rodriguez</h2>
+              <h2 className="font-bold">
+                {dataUser?.firstName + " " + dataUser?.lastName}
+              </h2>
               <p className="text-sm text-gray-400">Moviegoers</p>
             </div>
             <div className="px-8 my-5 pb-5">
               <button
-                type="submit" onClick={() => dispatch(logoutAction())}
+                type="submit"
+                onClick={() => dispatch(logoutAction())}
                 className="text-center bg-violet-800 py-2 px-10 rounded-md text-white"
               >
                 Logout
@@ -54,22 +91,31 @@ const Profilpage = () => {
             <div className="border-b pb-3 mb-7">
               <p className="font-bold">Details Information</p>
             </div>
-            <form>
+            <form onSubmit={updateUser}>
               <div className="flex gap-5">
                 <div className="basis-1/2 flex flex-col">
+                  {message && (
+                    <div className="w-full flex justify-center bg-green-500 my-2">
+                      <p className="text-white">{message}</p>
+                    </div>
+                  )}
                   <label>First Name</label>
                   <input
                     className="p-2 my-2 rounded-lg border border-slate-400 mb-4"
-                    type="email"
+                    type="text"
+                    name="firstName"
                     placeholder="Write your first name"
+                    defaultValue={dataUser.firstName}
                   />
                 </div>
                 <div className="basis-1/2 flex flex-col">
                   <label>Last Name</label>
                   <input
                     className="p-2 my-2 rounded-lg border border-slate-400 mb-4"
-                    type="email"
+                    type="text"
+                    name="lastName"
                     placeholder="Write your last name"
+                    defaultValue={dataUser.lastName}
                   />
                 </div>
               </div>
@@ -79,23 +125,38 @@ const Profilpage = () => {
                   <input
                     className="p-2 my-2 rounded-lg border border-slate-400 mb-4"
                     type="email"
+                    name="email"
                     placeholder="Write your email"
+                    defaultValue={dataUser.email}
                   />
                 </div>
                 <div className="basis-1/2 flex flex-col">
                   <label>Phone Number</label>
                   <input
                     className="p-2 my-2 rounded-lg border border-slate-400 mb-4"
-                    type="email"
+                    type="text"
+                    name="phoneNumber"
                     placeholder="Write your phone number"
+                    defaultValue={dataUser.phoneNumber}
                   />
                 </div>
               </div>
+              <button
+                type="submit"
+                disabled={isLoading}
+                className="bg-violet-800 text-white py-2 px-14 rounded-md mb-5"
+              >
+                Update changes
+              </button>
             </form>
           </div>
-          <button className="bg-violet-800 text-white py-2 px-14 rounded-md mb-5">
+          {/* <button
+            type="submit"
+            disabled={isLoading}
+            className="bg-violet-800 text-white py-2 px-14 rounded-md mb-5"
+          >
             Update changes
-          </button>
+          </button> */}
           <div className="bg-white rounded-md p-8 mb-5">
             <div className="border-b pb-3 mb-7">
               <p className="font-bold">Account and Privacy</p>
